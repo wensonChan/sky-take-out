f39ad7be363ca551174d257be586958eedb12410
wensonChan <wensoncwc@gmail.com>
2023-11-28 15:49:07 +0800

    删除购物车单一商品代码开发

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface ShoppingCartService {
     /**
      * 添加购物车
+     *
      * @param shoppingCartDTO
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
@@ -25,4 +26,11 @@ public interface ShoppingCartService {
      * @return
      */
     void cleanShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     *
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
