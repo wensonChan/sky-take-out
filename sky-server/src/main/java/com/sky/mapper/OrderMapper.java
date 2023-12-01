198b350fe6c049fcfbc3be7faa1614a56438096f
wensonChan <wensoncwc@gmail.com>
2023-12-01 15:49:36 +0800

    订单状态定时处理代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -63,4 +64,13 @@ public interface OrderMapper {
      */
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+    /**
+     * 根据订单状态和时间查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status =#{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTiimeLT(Integer status,LocalDateTime orderTime);
 }
